d837eab9f9a844cdc92f922917221d259b87889b
Eric Huang <eric.haoran.huang@gmail.com>
2020-11-03 12:05:25 -0500

    Working on the main menu

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import { Button } from 'react-bootstrap';
+import Form from "react-bootstrap/Form";
 
 interface Props {
+}
+
+function joinGameRoom() {
+    console.log("test")
 }
 
 export const MainMenu = (props: Props) => {
     return (
         <div>
-            <input type="text"></input>            
+            <Form>
+                <Form.Group>
+                    <Form.Label>Game Room Code:</Form.Label>
+                    <Form.Control type="text" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit" onClick={joinGameRoom}>Join</Button>
+
+            </Form>
+
+
         </div>
     )
 }
