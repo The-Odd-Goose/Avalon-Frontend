b9897a39b9aa65520f93b07ee15660dec8a6f199
Eric Huang <eric.haoran.huang@gmail.com>
2020-11-14 18:02:29 -0500

    Added in game room, will work on stuff

---
+++ src/components/GameRoom/GameRoom.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Chatbar} from "./Chatbar";
+import {Mission} from "./Mission";
+
+interface Props {
+    
+}
+
+// this game room will integrate the chat bar as well as the different missions and such
+export const GameRoom = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
