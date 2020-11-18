21c2358eaa8ddbe686fecfddab1b4c76bff683a6
Eric Huang <eric.haoran.huang@gmail.com>
2020-11-17 21:59:09 -0500

    Preparing for firebase integration

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -5,20 +5,25 @@ import Form from "react-bootstrap/Form";
 interface Props {
 }
 
-function joinGameRoom() {
-    console.log("test")
+function SignIn() {
 }
 
 export const MainMenu = (props: Props) => {
+
+    const joinGameRoom:(e: React.FormEvent<HTMLFormElement>) => void = async (e) => {
+        // here we'll add the user id to the specific game room
+
+    }
+
     return (
         <div>
-            <Form>
+            <Form onSubmit={joinGameRoom}>
                 <Form.Group>
                     <Form.Label>Game Room Code:</Form.Label>
-                    <Form.Control type="text" />
+                    <Form.Control type="text"/>
                 </Form.Group>
 
-                <Button variant="primary" type="submit" onClick={joinGameRoom}>Join</Button>
+                <Button variant="primary" type="submit">Join</Button>
 
             </Form>
 
