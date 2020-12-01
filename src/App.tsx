3150716c65c055439809f1695b905a7c2f9607fb
Eric Huang <eric.haoran.huang@gmail.com>
2020-11-30 21:20:28 -0500

    Got the sign in button to work

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
 import React from 'react';
 import './App.css';
 import { MainMenu } from './components/MainMenu';
-
-import firebase from "firebase/app";
-import 'firebase/firestore';
-import 'firebase/auth';
-
-firebase.initializeApp({
-  apiKey: "AIzaSyB-0O4MKOLCzVqgHWg1Ho93D4JvaVO6uRw",
-  authDomain: "the-odd-goose.firebaseapp.com",
-  databaseURL: "https://the-odd-goose.firebaseio.com",
-  projectId: "the-odd-goose",
-  storageBucket: "the-odd-goose.appspot.com",
-  messagingSenderId: "487313202863",
-  appId: "1:487313202863:web:5f793d2cdc66c1bdd07e5a",
-  measurementId: "G-KHHZH32NDE"
-})
+import { firebase, firestore, auth } from "./firebase-init";
 
 function App() {
   return (
--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -2,25 +2,41 @@ import React from 'react'
 import { Button } from 'react-bootstrap';
 import Form from "react-bootstrap/Form";
 
+import firebase from "firebase/app"
+import { auth } from '../firebase-init';
+import { useAuthState } from 'react-firebase-hooks/auth'
+
 interface Props {
 }
 
 function SignIn() {
+
+    const signInWithGoogle = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(provider);
+    }
+
+    return (
+        <button onClick={signInWithGoogle}>Sign in with Google</button>
+    )
+
 }
 
-export const MainMenu = (props: Props) => {
+function JoinGame() {
 
-    const joinGameRoom:(e: React.FormEvent<HTMLFormElement>) => void = async (e) => {
+    const joinGameRoom: (e: React.FormEvent<HTMLFormElement>) => void = async (e) => {
         // here we'll add the user id to the specific game room
 
     }
 
+
     return (
         <div>
             <Form onSubmit={joinGameRoom}>
+
                 <Form.Group>
                     <Form.Label>Game Room Code:</Form.Label>
-                    <Form.Control type="text"/>
+                    <Form.Control type="text" />
                 </Form.Group>
 
                 <Button variant="primary" type="submit">Join</Button>
@@ -29,5 +45,17 @@ export const MainMenu = (props: Props) => {
 
 
         </div>
+    )
+
+}
+
+export const MainMenu = (props: Props) => {
+
+    const [user] = useAuthState(auth);
+
+    return (
+        <>
+            {user ? <JoinGame /> : <SignIn />}
+        </>
     )
 }
