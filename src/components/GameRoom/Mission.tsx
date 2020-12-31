c375f6a1f84d588e42bfd806609ccda3d3bb47f2
Eric Huang <eric.haoran.huang@gmail.com>
2020-12-31 11:18:42 -0500

    Finished?

--- src/components/GameRoom/Mission.tsx
+++ src/components/GameRoom/Mission.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Spinner, Image, Badge, Button, Alert } from 'react-bootstrap';
+import { Spinner, Image, Badge, Button, Alert, Modal } from 'react-bootstrap';
 import { createPostRequest } from '../fetch';
 
 interface Props {
@@ -107,7 +107,8 @@ interface VoteProps {
     mission: Array<string>,
     vote: Array<string>,
     uid: string,
-    players: Map<String, any>
+    players: Map<String, any>,
+    gameId: string
 }
 
 const displayPlayer = (player: any, key: number): JSX.Element => {
@@ -123,15 +124,78 @@ const displayPlayer = (player: any, key: number): JSX.Element => {
     return <></>;
 }
 
-const VoteMission = (props: VoteProps) => {
+interface VoteFormProps {
+    gameId: string,
+    uid: string,
+    voteField: string,
+    stringFor: string,
+    stringAgainst: string,
+    path: string
+}
 
-    const { voteFor, voteAgainst, mission, vote, players, uid } = props;
+const Vote = (props: VoteFormProps) => {
+    const { gameId, uid, voteField, stringFor, stringAgainst, path } = props;
 
-    const canVote = vote.reduce((prev, current) => {
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState("")
+    const [show, setShow] = useState({ show: false, voteFor: false })
+
+    const voteOnMission = async () => {
+        setLoading(true)
+        const response = await createPostRequest({ gameId, uid, [voteField]: show.voteFor }, path)
+        if (typeof response === 'string') {
+            // ie there is an error,
+            setError(response)
+        }
+        setLoading(false)
+        setShow({ show: false, voteFor: false })
+    }
+
+    const handleClick = (voteFor: boolean) => {
+        setShow({ show: true, voteFor })
+    }
+
+    const handleClose = () => setShow((state) => ({ show: false, voteFor: state.voteFor }))
+
+    return (
+        <>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Button variant="primary" onClick={() => handleClick(true)}>{stringFor}</Button>
+            <Button variant="danger" onClick={() => handleClick(false)}>{stringAgainst}</Button>
+
+            <Modal
+                show={show.show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>{show.voteFor ? stringFor : stringAgainst}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>{loading ? "...loading" : "Are you sure you want to vote this way?"}</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>Cancel</Button>
+                    <Button variant="primary" onClick={voteOnMission}>Yes</Button>
+                </Modal.Footer>
+            </Modal>
+
+        </>
+    )
+
+}
+
+// finds if uid is in array vote
+const getCanVote = (vote: Array<any>, uid: string): boolean => {
+    return vote.reduce((prev, current) => {
         return prev || uid === current
     }, false)
+}
 
-    console.log(canVote)
+const VoteMission = (props: VoteProps) => {
+
+    const { voteFor, voteAgainst, mission, vote, players, uid, gameId } = props;
+
+    const canVote = getCanVote(vote, uid)
 
     return (
         <>
@@ -140,6 +204,12 @@ const VoteMission = (props: VoteProps) => {
                 return displayPlayer(players.get(player), key)
             })}
 
+            <hr />
+            {vote && "Players left to vote:" &&
+                vote?.map((player, key) => {
+                    return displayPlayer(players.get(player), key)
+                })
+            }
             <hr />
             Voted For: {" "}
             {voteFor?.map((player, key) => {
@@ -153,7 +223,43 @@ const VoteMission = (props: VoteProps) => {
             })}
 
             <hr />
-            
+            {canVote &&
+                <Vote
+                    gameId={gameId}
+                    uid={uid}
+                    voteField="voteFor"
+                    path="/voteMission"
+                    stringAgainst="Vote Against Mission"
+                    stringFor="Vote For Mission"
+                />}
+        </>
+    )
+}
+
+interface DecideMissionProps {
+    mission: Array<any>,
+    uid: string,
+    gameId: string
+}
+
+const DecideMission = (props: DecideMissionProps) => {
+    const { mission, uid, gameId } = props;
+    const canVote = getCanVote(mission, uid)
+
+    return (
+        <>
+            {canVote &&
+                <>Decide on mission!
+                    <Vote
+                        gameId={gameId}
+                        uid={uid}
+                        voteField="vote"
+                        path="/choose"
+                        stringAgainst="Fail Mission"
+                        stringFor="Succeed Mission"
+                    />
+                </>
+            }
         </>
     )
 }
@@ -172,10 +278,23 @@ const getJsxArray = (num: Number, upperNum: number, jsxTrue: (key: number) => JS
     return jsxArray;
 }
 
+interface MissionChoicesProps {
+    successMission: number,
+    failMission: number
+}
+
+const MissionChoices = (props: MissionChoicesProps) => {
+    const {successMission, failMission} = props;
+
+    return (
+        <>Votes for success: {successMission} {" "} Votes for fail: {failMission}</>
+    )
+}
+
 export const Mission = (props: Props) => {
 
     const { gameData, missionMaker, players, uid, gameId, playersDict } = props;
-    const { success, turn, fail, rejected: rejections, voteFor, voteAgainst, vote, mission } = gameData
+    const { success, turn, fail, rejected: rejections, voteFor, voteAgainst, vote, mission, successMission, failMission } = gameData
 
     // turns the number of successes into an array of jsx elements
     // 3 - that number gets turned into a different type
@@ -193,14 +312,27 @@ export const Mission = (props: Props) => {
 
             <hr />
 
-            {(turn % 10 === 0) && missionMaker === uid && // if the turn is x1 and we are the mission maker, show choosing mission
-                <>Possible Mission People:
-                    <ChooseMission players={players} turn={turn} uid={missionMaker} gameId={gameId} />
+            {(turn % 10 === 0) && (turn < 60) &&
+
+                <>
+                    {turn >= 20 && 
+                        <MissionChoices successMission={successMission} failMission={failMission}/>
+                    }
+                    {missionMaker === uid && // if the turn is x1 and we are the mission maker, show choosing mission
+                        <>
+                            Possible Mission People:
+                            <ChooseMission players={players} turn={turn} uid={missionMaker} gameId={gameId} />
+                        </>
+                    }
                 </>
             }
 
-            {(turn % 10 === 1) &&
-                <VoteMission uid={uid} voteFor={voteFor} voteAgainst={voteAgainst} vote={vote} mission={mission} players={playersDict} />
+            {(turn % 10 === 1 || (turn % 10 === 5)) &&
+                <VoteMission gameId={gameId} uid={uid} voteFor={voteFor} voteAgainst={voteAgainst} vote={vote} mission={mission} players={playersDict} />
+            }
+
+            {(turn % 10 === 5) &&
+                <DecideMission gameId={gameId} uid={uid} mission={mission} />
             }
 
         </div>
