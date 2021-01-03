e6ab1b2606611c633abb358ae91ddc0d014408f8
Eric Huang <eric.haoran.huang@gmail.com>
2021-01-03 12:41:10 -0500

    Rechanged the api key

--- src/firebase-init.tsx
+++ src/firebase-init.tsx
@@ -1,19 +1,18 @@
 import firebase from "firebase/app";
 import 'firebase/firestore';
 import 'firebase/auth';
-
-firebase.initializeApp({
+var firebaseConfig = {
   apiKey: "AIzaSyB-0O4MKOLCzVqgHWg1Ho93D4JvaVO6uRw",
   authDomain: "the-odd-goose.firebaseapp.com",
   databaseURL: "https://the-odd-goose.firebaseio.com",
   projectId: "the-odd-goose",
   storageBucket: "the-odd-goose.appspot.com",
   messagingSenderId: "487313202863",
-  appId: "1:487313202863:web:5f793d2cdc66c1bdd07e5a",
-  measurementId: "G-KHHZH32NDE"
-})
+  appId: "1:487313202863:web:b211a402a629a6b7d07e5a",
+  measurementId: "G-MWQQ6LZRSS"
+};
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
-export {firebase, auth, firestore};
+export { firebase, auth, firestore };
